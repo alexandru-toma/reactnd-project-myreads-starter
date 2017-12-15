82711846fead29a4e086a63e2f49becce3db28a8
Alexandru Nicu Toma <alexandru.toma.nicu@gmail.com>
2017-12-15 17:28:53 +0200

    add routing based on url

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
+import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks/>
-        ) : (
-          <div className="list-books">
+        <Route
+          exact
+          path="/"
+          render= {() => (
+            <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
@@ -179,10 +171,15 @@ class BooksApp extends React.Component {
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
-        )}
+          )}
+        />
+        <Route
+          path="/search"
+          component={SearchBooks}
+        />
       </div>
-    )
+      )}
   }
-}
+
 
 export default BooksApp
