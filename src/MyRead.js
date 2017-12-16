8428698e13b759b84240f7148829093b6470e5f6
Alexandru Nicu Toma <alexandru.toma.nicu@gmail.com>
2017-12-16 14:35:16 +0200

    add link to search button

--- src/MyRead.js
+++ src/MyRead.js
@@ -2,13 +2,27 @@ import React from 'react';
 import CurrentlyReading from './MyRead/CurrentlyReading';
 import WantToRead from './MyRead/WantToRead';
 import Read from './MyRead/Read';
+import {Link} from 'react-router-dom';
 
 const MyRead = () => {
-    return(
+    return (
         <div>
-            <CurrentlyReading/>
-            <WantToRead/>
-            <Read/>
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <CurrentlyReading />
+                    <WantToRead />
+                    <Read />
+                </div>
+            </div>
+            <div className="open-search">
+                <Link 
+                    to="/search"
+                >Add a book
+                </Link>
+             </div>
         </div>
     );
 }
