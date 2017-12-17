82299ef6c9720d1908c59c5f4ad3d668bf415112
Alexandru Nicu Toma <alexandru.toma.nicu@gmail.com>
2017-12-17 10:55:16 +0200

    pass data to myRead page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import MyRead from './MyRead';
 import {Route} from 'react-router-dom';
 
 class BooksApp extends React.Component {
+  state = {
+    books: {}
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    });
+  }
+  
   render() {
     return (
       <div className="app">
         <Route
           exact
           path="/"
-          component={MyRead}
+          render={() => (
+            <MyRead
+              books={this.state.books}
+            />
+          )}
         />
         <Route
           path="/search"
